cb84f8c5ee7f5f790266b86d2fb4cc70f3d1f14c
Meltemmmm <meltemball@hotmail.com>
2023-12-10 17:27:37 +0300

    first commit

---
+++ src/PR01_FirstClass.java
@@ -0,0 +1,7 @@
+public class PR01_FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Sistem deneme");
+
+    }
+}
---
+++ src/PR03_ThirdClass.java
@@ -0,0 +1,7 @@
+public class PR03_ThirdClass {
+    public static void main(String[] args) {
+
+        System.out.println("Son deneme");
+
+    }
+}
